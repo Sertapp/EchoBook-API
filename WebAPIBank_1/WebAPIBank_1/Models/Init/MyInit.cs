06f54573034db85904efed6947608158254251a3
Sertap Ugur <77464266+Sertapp@users.noreply.github.com>
2022-06-11 01:16:40 +0300

    APIBank

---
+++ WebAPIBank_1/WebAPIBank_1/Models/Init/MyInit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank_1.Models.Context;
+using WebAPIBank_1.Models.Entities;
+
+namespace WebAPIBank_1.Models.Init
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInfo ci = new CardInfo();
+            ci.CardUserName = "Sertap Ugur";
+            ci.CardNumber = "1111 1111 1111 1111";
+            ci.CardExpiryYear = 2023;
+            ci.CardExpiryMonth = 12;
+            ci.SecurityNumber = "222";
+            ci.Limit = 20000;
+            ci.Balance = 20000;
+            context.Cards.Add(ci);
+            context.SaveChanges();
+        }
+    }
+}
